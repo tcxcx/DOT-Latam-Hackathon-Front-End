cb69e7649172d06afa324422aad9d7db7e9976b4
tcxcx <72812526+tcxcx@users.noreply.github.com>
2022-11-08 09:15:53 -0500

    add project details

--- Latam_Hackathon_/src/Components/Main_project_1/Main_project_1.jsx
+++ Latam_Hackathon_/src/Components/Main_project_1/Main_project_1.jsx
@@ -1,86 +1,27 @@
 import React, {useEffect} from 'react'
-import './main.css'
-import {HiOutlineLocationMarker} from 'react-icons/hi'
-import {HiClipboardList} from 'react-icons/hi'
-import img from '../Assets/img_mochasa.jpg'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import './main.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import Mochasa from '../Main_project_1/card_mochasa';
 
 
-const Data = [
-  {
-  id:1,
-  imgSrc: img,
-  destTitle: 'Guayaquil',
-  location: 'Ecuador',
-  grade: 'MOCHASA ',
-  fees: '200 kW',
-  description: 'Mochasa is a company with over 50+ years in the Shrimp Feedstock Industry looking to become more sustainable.' 
-  },
-]
-
-const Main_project = () => {
+const Main_project = ({ data }) => {
   useEffect(()=>{
     Aos.init({duration: 4000})
  }, [])
+
   return (
-    <section id='main' className='main section container'>
+    <section id='main' className='main section container' data-aos="fade-up">
       <div className="secTitle">
         <h3 className="title">
           Project overview
         </h3>
       </div>
-
-      <div className="secContent grid">
-        {
-          Data.map(({id, imgSrc, destTitle, location, grade, fees, description})=>{
-            return (
-              
-              <div key={id} data-aos="fade-up" className="singleDestination">
-      
-                 <div className="imageDiv">
-                 <img src={imgSrc} alt="" />
-                 </div>
-      
-                <div className="cardInfo">
-                 <h4 className="destTitle">{destTitle}</h4>
-                 <span className="continent flex">
-                    <HiOutlineLocationMarker className="icon"/>
-                    <span className="name">{location}</span>
-                 </span>
-      
-                 <div className="fees flex">
-                    <div className="grade">
-                      <span>{grade}<small>PPA </small> </span>
-                    </div>
-                    <div className="price">
-                      <h5>{fees}</h5>
-                    </div>
-                 </div>
-      
-                 <div className="desc">
-                  <p>{description}</p>
-                 </div>
-      
-                 <button className='btn flex'>DETAILS <HiClipboardList className="icon"/> </button>
-                </div>
-              </div>
-              
-      
-            )
-          }) 
-        }
-      </div>
-      <div className="secTitle">
-        <h3 className="title">
-          Legal Documentation
-        </h3>
-      </div>
-      
-
-      
+      {<Mochasa />}
     </section>
-  )
-}
+  
+    )
+  }
+
 
 export default Main_project
---
+++ Latam_Hackathon_/src/Components/Main_project_1/card_mochasa.js
@@ -0,0 +1,31 @@
+import React, {useEffect} from 'react'
+import './card_mochasa.css'
+import img from '../Assets/img_mochasa.jpg'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+const Mochasa = () => {
+    useEffect(()=>{
+        Aos.init({duration: 4000})
+     }, [])
+return(
+    <div class="tarjeta">
+        <div class="contenido">
+            <div class="ladoIzq">
+                <h2 class="titulo">Molinos Champion S.A. Mochasa</h2>
+                <h4 className="destTitle">Guayaquil, Ecuador</h4>
+                    <div class="cuerpo">
+                    <p>Mochasa is a company with over 50+ years in the Shrimp Feedstock Industry looking to become more sustainable.</p>
+                    <a class="btn" href="http://www.molinoschampion.com/">Webpage</a>
+                    <a class="btn" href="#">Buy E-NFT</a>
+                    </div>
+                </div>
+                <div class="ladoDer">
+                    <img className="image-mochasa" alt="" src={img} />
+            </div>
+        </div>
+    </div>
+ );
+};
+
+export default Mochasa;
